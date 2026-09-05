960101a1e90b2f2a9aa3acfb356ac7325701e738
TSOTK-0469 <3303409505@qq.com>
2026-09-05 22:52:25 +0800

    Release Yomu 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "cn.yomu.reader"
         minSdk = 23
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.0-beta.1"
+        versionCode = 5
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
